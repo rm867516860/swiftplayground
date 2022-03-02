1f5db3d4b7b317043c841e1abefe52f67a2dba39
rm <ruanmin@agora.io>
2022-03-02 11:23:54 +0800

    Initial Commit

---
+++ swiftplayground/swiftplaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftplaygroundApp.swift
+//  swiftplayground
+//
+//  Created by RM on 2022/3/2.
+//
+
+import SwiftUI
+
+@main
+struct swiftplaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
