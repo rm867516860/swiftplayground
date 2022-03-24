f5180746c1f2d96de7dc3fa3fd02d7d61dc40035
rm <ruanmin@agora.io>
2022-03-24 10:40:56 +0800

    update

--- test04_count.playground/Contents.swift
+++ test04_count.playground/Contents.swift
@@ -1,26 +1,25 @@
 import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+//闭包，把函数简化
+//http://www.swift51.com/swift5.1/02_language_guide/07_Closures.html
 
-func pass100 (f:(Int)->()) {
-    f(100)
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+//函数表达
+func backword (_ s1 : String, s2 : String) -> Bool{
+    
+    return s1 > s2
 }
 
-//func countAdder(f:()->()) -> () -> () {
-//    var ct = 0
-//    return {
-//        ct = ct + 1
-//        print("count is \(ct)")
-//        f()
-//    }
-//}
-//
-//func greet () {
-//    print("howdy")
-//}
-//
-//
-//let countedGreet = countAdder(greet)
-//countedGreet()
-//countedGreet()
-//countedGreet()
+
+//闭包表达形式
+var reversename = names.sorted(by: {(s1 : String, s2: String) -> Bool in
+    return s1 > s2
+})
+
+
+
+let back = names.sorted(by: backword)
+print(back)
+print(reversename)
